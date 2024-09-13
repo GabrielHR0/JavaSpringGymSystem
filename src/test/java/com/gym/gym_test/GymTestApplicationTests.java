ae7f27e840b764b35203b4f15c5e8a6e7bf811f2
Gabriel <gabrielhenriqueojr@gmail.com>
2024-09-13 17:55:30 -0300

    initial commit

---
+++ src/test/java/com/gym/gym_test/GymTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gym.gym_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
